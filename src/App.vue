31ba703452793606cc4fe9e79fbbf6e25c4753fd
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-18 12:45:46 +0100

    completata Milestone_1

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,49 @@
 
      <header>
       <div class="container">
+
         <div class="row">
+
           <div class="col-12">
             <input v-model="userQuery" type="text" placeholder="search" id="search">
             <button @click="startSearch">
               search
             </button>
           </div>
+
         </div>
+        <!-- search form -->
+
+        <div class="searchResult">
+          
+          <div v-for="film in userFilms" :key="film.id" class="film">
+            <p>
+              TITOLO: {{film.title}}
+            </p>
+            <!--  -->
+
+            <p>
+              TITOLO ORIGINALE: {{film.original_title}}
+            </p>
+            <!--  -->
+
+            <p>
+              LINGUA: {{film.original_language}}
+            </p>
+            <!--  -->
+
+            <p>
+              VOTO: {{film.vote_average}}
+            </p>
+            <!--  -->
+            
+            <p>
+              --------------------------------------- <br>
+            </p>
+          </div>
+
+        </div>
+        <!--  -->
       </div>
   </header>
     <!--  -->
@@ -26,13 +61,15 @@ export default {
     data() {
       return {
         userQuery: "",
+        userFilms: [],
       }
     },
     methods: {
       startSearch(){
         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=d96fb085a5d3052af443a4f202383b1f&query=${this.userQuery}&page=1`)
             .then(r => {
-              console.log(r);
+              console.log(r.data.results);
+              this.userFilms = r.data.results;
             })
       }
     },
