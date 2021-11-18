09c4ac15583d8c33781a48131703be3f7742d2d0
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-18 12:32:56 +0100

    eseguita richiesta axios con dati inseriti dall'utente

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,41 @@
 <template>
   <div id="app">
 
-    <site_header :start-search="startSearch(param)"/>
+     <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <input v-model="userQuery" type="text" placeholder="search" id="search">
+            <button @click="startSearch">
+              search
+            </button>
+          </div>
+        </div>
+      </div>
+  </header>
     <!--  -->
-
-    <site_main/>
-    <!--  -->
-
-    <site_footer/>
 
   </div>
 </template>
 
 <script>
-import site_header from "./components/site_header.vue";
-import site_main from "./components/site_main.vue";
-import site_footer from "./components/site_footer.vue";
-
+import axios from "../node_modules/axios"
 export default {
   name: 'App',
-    components: {
-      site_header,
-      site_main,
-      site_footer
-    },
+
     data() {
       return {
         userQuery: "",
       }
     },
     methods: {
-      startSearch(param){
-        console.log(param);
+      startSearch(){
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=d96fb085a5d3052af443a4f202383b1f&query=${this.userQuery}&page=1`)
+            .then(r => {
+              console.log(r);
+            })
       }
-    }
+    },
 }
 </script>
 
